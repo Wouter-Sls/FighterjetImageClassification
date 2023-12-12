5c23a9d0bfef9321bad56e631c165b99d1e8c8fe
Wouter Selis <r0953028@student.thomasmore.be>
2023-12-12 16:16:21 +0100

    Final

--- Fighterjets-Streamlit.py
+++ Fighterjets-Streamlit.py
@@ -7,6 +7,7 @@ import torch
 st.title("Fighterjets image classification")
 st.subheader(':blue[_Created by Wouter Selis & Kieran Cornelissen & Gilles Witters_] :male-technologist:', divider='rainbow')
 
+st.write("In this streamlit app you can test our classification model for fighterjets. The goal of this project is to classify 5 different fighterjets: F-1117 Nighthawk, F-16 Fighting Falcon, F-22 Raptor, F-4 Phantom, MiG-29 Fulcrum. We also compared our model with Google Teachable Machine and you can see the results below. Our model is created with fastAi and resnet50. To test our model you can upload an image and view the prediction and how certain our model is.")
 
 #image uploader to predict
 allowed_types = ("jpg", "jpeg", "png")
@@ -24,46 +25,52 @@ if uploaded_file is not None:
     with st.spinner('Classifying...'):
         #predict image
         predictions=loaded_model.predict(image)
+
+        prediction = f"{predictions[0].replace('_', ' ')}"
+        st.write(f"Our model is {round(float(torch.max(predictions[2])*100))}% certain it is a {prediction}.")
         
-        st.write(f"Prediction: {predictions[0].replace('_', ' ')}")
-        st.write(f"Precision: {round(float(torch.max(predictions[2])*100))}%")
-    
-    #Compare confusion matrix
-    st.write("")
-    st.subheader("Confusion matrix models: ")
-    st.write("")
-    
+    st.divider()
 
-    st.write("Our model:")
-    st.image('./images/CM_OurModel.png')
-    st.write("Accuracy per class:")
-    st.write("F-1117_Nighthawk: 96%")
-    st.write("F-16_Fighting_Falcon: 81%")
-    st.write("F-22_Raptor: 84%")
-    st.write("F-4_Phantom: 71%")
-    st.write("MiG_29_Fulcrum: 90%")
-    st.write("")
+#Compare confusion matrix
+st.write("")
+st.subheader("Confusion matrix models: ")
+st.write("")
 
-    st.write("Google teachable machine model:")
-    st.image('./images/CM_GTM.png')
-    st.write("Accuracy per class:")
-    st.write("F-1117_Nighthawk: 93%")
-    st.write("F-16_Fighting_Falcon: 58%")
-    st.write("F-22_Raptor: 78%")
-    st.write("F-4_Phantom: 77%")
-    st.write("MiG_29_Fulcrum: 80%")
-    st.write("")
-    
-    #Compare losses
-    st.write("")
-    st.subheader("Losses: ")
-    st.write("")
-    
+st.write("Here we compare the confusion matrix from our model with the confusion matrix from Google Teachable Machine. As you can see the accuracy of our model is in all cases the better one in classifying the planes except for the F-4 Phantom. In general our model is more accurate than Google Teachable Machine.")
 
-    st.write("Our loss per epoch:")
-    st.image('./images/Loss_OurModel.png')
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("**Our model:**")
+        st.image('./images/CM_OurModel.png')
+        st.write("Accuracy per class:")
+        lst=["F-1117 Nighthawk: 96%","F-16 Fighting Falcon: 81%","F-22 Raptor: 84%","F-4 Phantom: 71%","MiG-29 Fulcrum: 90%"]
+        for i in lst:
+            st.markdown("- " + i)
+       
+    with col2:
+        st.write("**Google teachable machine model:**")
+        st.image('./images/CM_GTM.png')
+        st.write("Accuracy per class:")
+        lst_GTM=["F-1117 Nighthawk: 93%","F-16 Fighting Falcon: 58%","F-22 Raptor: 78%","F-4 Phantom: 77%","MiG-29 Fulcrum: 80%"]
+        for i in lst_GTM:
+            st.markdown("- " + i)
 
-    st.write("Google teachable machine losses per epoch:")
-    st.image('./images/Loss_GTM.png')
+
+st.divider()
+#Compare losses
+st.write("")
+st.subheader("Losses: ")
+st.write("")
+st.write("Here we can compare the loss of the models. The difference between our loss and the Google Teachable Machine loss is that our validation loss stabilize and slightly descents but with Google Teachable Machine it slightly increases. When the validation loss starts to increase this most likely means that the model is overfitted. We can also see that our final validation loss ends on +-0.6 and Google Teachable Machine's validation loss ends on +-0.8.")
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("**Our loss per epoch:**")
+        st.image('./images/Loss_OurModel.png')
+    with col2:
+        st.write("**Google teachable machine losses per epoch:**")
+        st.image('./images/Loss_GTM.png')
 
 
