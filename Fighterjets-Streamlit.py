d1f63b03aad6aa3b2a69f60392e51f40fb91a8a3
Wouter Selis <r0953028@student.thomasmore.be>
2023-12-12 10:52:16 +0100

    streamlit app

--- Fighterjets-Streamlit.py
+++ Fighterjets-Streamlit.py
@@ -1,9 +1,42 @@
 import streamlit as st
-import fastbook
-from fastbook import *
+from PIL import Image
+from fastai.vision.all import load_learner, ClassificationInterpretation
+import torch
 
 #Title
 st.title("Fighterjets image classification")
 st.subheader(':blue[_Created by Wouter Selis & Kieran Cornelissen & Gilles Witters_] :male-technologist:', divider='rainbow')
 
-fastbook.setup_book()
+
+#image uploader to predict
+allowed_types = ("jpg", "jpeg", "png")
+uploaded_file = st.file_uploader("Choose an image...", type=allowed_types)
+
+#load model
+loaded_model = load_learner("models/model50")
+
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+    #predicting image
+    with st.spinner('Classifying...'):
+        #predict image
+        predictions=loaded_model.predict(image)
+        
+        st.write(f"Prediction: {predictions[0].replace('_', ' ')}")
+        st.write(f"Precision: {round(float(torch.max(predictions[2])*100))}%")
+    
+    #Compare confusion matrix
+    st.write("")
+    st.subheader("Confusion matrix models: ")
+    st.write("")
+
+    st.write("Our model")
+    st.image()
+
+    st.write("Google teachable machine model")
+    st.image()
+
+
